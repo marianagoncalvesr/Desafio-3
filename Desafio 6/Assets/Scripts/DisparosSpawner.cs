daaf588071248da573d374e4190f0e2d96efb6d0
marianagoncalvesr <79268306+marianagoncalvesr@users.noreply.github.com>
2021-10-28 12:20:45 -0300

    desafio 6

---
+++ Desafio 6/Assets/Scripts/DisparosSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparosSpawner : MonoBehaviour
+{
+    public GameObject[] objetoPrefab;
+    public Transform arma;
+    public static int bulletCount = 0;
+    AudioSource disparo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        disparo = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ///Con espacio instanciamos las balas
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (bulletCount < 15) 
+            {
+                Disparo();
+                bulletCount++;
+            }
+        }
+    }
+
+    void Disparo()
+    {
+       
+        int disparoPlayer = Random.Range(0, objetoPrefab.Length);
+        Instantiate(objetoPrefab[disparoPlayer], transform.position, transform.rotation);
+        disparo.PlayOneShot(disparo.clip);    
+    }
+
+}
+
---
+++ Desafio 6/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speedPlayer = 0.5f;
+    public GameObject camaraActivate;
+    Vector3 personajeLimite = new Vector3(-0f, 1.8f - 12f);
+    AudioSource steps;
+    float ejeHorizontal;
+    public float velocidadRotacion = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(-14f, 1.8f, -12f);
+        steps = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    private void LateUpdate()
+    {
+        Movement();
+    }
+
+    //Movimiento del personaje
+    void Movement()
+    {
+        ejeHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movementPlayer = new Vector3(0, 0, 0);
+        transform.Translate(speedPlayer * Time.deltaTime * movementPlayer, Space.World);
+        this.transform.Rotate(Vector3.up * ejeHorizontal * velocidadRotacion * Time.deltaTime);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * speedPlayer;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * -1 * Time.deltaTime * speedPlayer;
+        }
+    }
+}
