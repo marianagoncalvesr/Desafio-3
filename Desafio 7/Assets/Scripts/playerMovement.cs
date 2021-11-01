c31e745997c8a9c1f38d1890b96acbdc34a0de4b
marianagoncalvesr <79268306+marianagoncalvesr@users.noreply.github.com>
2021-10-31 23:35:54 -0300

    cambios

---
+++ Desafio 7/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] int playerSpeed = 5;
+    [SerializeField] int rotationSpeed = 5;
+   // public bool size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float ejeH = Input.GetAxis("Horizontal");
+        float ejeV = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(ejeH, 0, ejeV);
+        playerMovement.Normalize();
+        transform.Translate(playerSpeed * Time.deltaTime * playerMovement, Space.World);
+
+        if (playerMovement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(playerMovement, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+    }
+    public void ChangeSize()
+    {
+        Debug.Log("playermovement script funcionando");
+        //transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+}
